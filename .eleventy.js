a8c311f237e1727287ee54ec0600a246df5260fc
Tristan Matthias <hello@tristanmatthias.com>
2020-06-08 16:16:17 -0400

    feat: added sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const tagList = require('./_shortcodes/tagList');
 const htmlMetaCodes = require('./_shortcodes/seo');
 const articleContent = require('./_filters/articleContent');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
 
 
 module.exports = function (eleventyConfig) {
@@ -44,6 +45,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("tagList", tagList);
   eleventyConfig.addFilter("articleContent", articleContent);
   eleventyConfig.addFilter("json", v => JSON.stringify(v[0].url));
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
 
   eleventyConfig.addPlugin(pluginRss);
 
